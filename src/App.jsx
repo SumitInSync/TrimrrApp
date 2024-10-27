12bfb939994317158eb424566e7353060a259315
SumitInSync <154971556+SumitInSync@users.noreply.github.com>
2024-10-27 15:44:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,61 @@
-import { useState } from 'react';
-import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import AppLayout from './layouts/AppLayout';
-import Landing from './pages/Landing';
-import DashBoard from './pages/DashBoard';
-import Auth from './pages/Auth';
-import RedirectLink from './pages/RedirectLink';
-import Link from './pages/Link';
-import UrlProvider from './context';
-import RequireAuth from './components/require-auth';
-import {Suspense} from 'react';
+import { Suspense } from "react";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AppLayout from "./layouts/AppLayout";
+import Landing from "./pages/Landing";
+import DashBoard from "./pages/DashBoard";
+import Auth from "./pages/Auth";
+import RedirectLink from "./pages/RedirectLink";
+import Link from "./pages/Link";
+import UrlProvider from "./context";
+import RequireAuth from "./components/require-auth";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    children: [  // changed 'Children' to 'children'
+    children: [
       {
-        path: '/',
+        path: "/",
         element: <Landing />
       },
       {
-        path: '/dashboard',
-        element: <RequireAuth>  <DashBoard /> </RequireAuth>
-       
+        path: "/dashboard",
+        element: (
+          <RequireAuth>
+            <DashBoard />
+          </RequireAuth>
+        )
       },
       {
-        path: '/auth',
+        path: "/auth",
         element: <Auth />
       },
       {
-        path: '/link/:id',
-        element: <RequireAuth><Link /></RequireAuth>
+        path: "/link/:id",
+        element: (
+          <RequireAuth>
+            <Link />
+          </RequireAuth>
+        )
       },
       {
-        path: '/:id',
-        element: 
-        <Suspense fallback={<div>Loading...</div>}>
+        path: "/:id",
+        element: (
+          <Suspense fallback={<div>Loading...</div>}>
             <RedirectLink />
-          </Suspense>
-      
-        
-      },
+          </Suspense>
+        )
+      }
     ]
   }
 ]);
 
 function App() {
-  return <UrlProvider>
-    <RouterProvider router={router} />;
-  </UrlProvider>
-  
+  return (
+    <UrlProvider>
+      <RouterProvider router={router} />
+    </UrlProvider>
+  );
 }
 
 export default App;
